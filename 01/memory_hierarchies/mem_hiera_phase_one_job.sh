2fe4dee67cfc13daf47388e96669378ecabc1a80
Basil Zemann <bzemann@student.ethz.ch>
2024-02-26 14:08:33 +0100

    small change

--- 01/memory_hierarchies/mem_hiera_phase_one_job.sh
+++ 01/memory_hierarchies/mem_hiera_phase_one_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=slurm_job_one      # Job name    (default: sbatch)
-#SBATCH --output=slurm_job_one-%j.out # Output file (default: slurm-%j.out)
-#SBATCH --error=slurm_job_one-%j.err  # Error file  (default: slurm-%j.out)
+#SBATCH --job-name=mem_one_job        # Job name    (default: sbatch)
+#SBATCH --output=mem_job_one-%j.out   # Output file (default: slurm-%j.out)
+#SBATCH --error=mem_job_one-%j.err    # Error file  (default: slurm-%j.out)
 #SBATCH --ntasks=1                    # Number of tasks
 #SBATCH --cpus-per-task=1             # Number of CPUs per task
 #SBATCH --mem-per-cpu=1024            # Memory per CPU
