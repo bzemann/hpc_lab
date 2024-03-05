7f6ee70d3e6c8ea6b944b798bb676d1521d9f4b6
Basil Zemann <bzemann@eu-login-10.euler.ethz.ch>
2024-03-05 14:07:31 +0100

    updated run.sh

--- project1_part_a/bandwith_STREAM/run.sh
+++ project1_part_a/bandwith_STREAM/run.sh
@@ -1,8 +1,8 @@
 #load required moduels
 module load gcc
 
-ARRAY_SIZE_ONE=8000000
-ARRAY_SIZE_TWO=16000000
+ARRAY_SIZE_ONE=9600000
+ARRAY_SIZE_TWO=19200000
 
 #Euler VII p1
 gcc -O3 -march=native -DSTREAM_TYPE=double -DSTREAM_ARRAY_SIZE=${ARRAY_SIZE_ONE} -DNTIMES=20 stream.c -o stream_1
@@ -10,4 +10,4 @@ sbatch --constraint=EPYC_7H12 --mem-per-cpu=2G --output=stream_1.out --wrap "./s
 
 #Euler VII p2
 gcc -O3 -march=native -DSTREAM_TYPE=double -DSTREAM_ARRAY_SIZE=${ARRAY_SIZE_TWO} -DNTIMES=20 stream.c -o stream_2
-sbatch --constraint=EPYC_7H12 --mem-per-cpu=2G --output=stream_2.out --wrap "./stream_2"
+sbatch --constraint=EPYC_7763 --mem-per-cpu=2G --output=stream_2.out --wrap "./stream_2"
