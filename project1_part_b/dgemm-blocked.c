d684ae8b4440f0318d50f336808711b9cbba71e1
Basil Zemann <bzemann@student.ethz.ch>
2024-03-08 10:55:22 +0100

    size 100

--- project1_part_b/dgemm-blocked.c
+++ project1_part_b/dgemm-blocked.c
@@ -26,7 +26,7 @@ void square_dgemm(int n, double *A, double *B, double *C) {
   // }
 
   // blocked matrix multiplication
-  int block_size = 37;
+  int block_size = 100;
   for (int i = 0; i < n; i += block_size) {
     for (int j = 0; j < n; j += block_size) {
       for (int k = 0; k < n; k += block_size) {
