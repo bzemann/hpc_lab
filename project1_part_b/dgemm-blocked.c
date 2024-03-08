aa1250674278513300e3879a1a11a8d157fffa34
Basil Zemann <bzemann@student.ethz.ch>
2024-03-08 11:04:38 +0100

    size 200

--- project1_part_b/dgemm-blocked.c
+++ project1_part_b/dgemm-blocked.c
@@ -13,20 +13,11 @@ const char *dgemm_desc = "Blocked dgemm.";
  * On exit, A and B maintain their input values.
  */
 void square_dgemm(int n, double *A, double *B, double *C) {
-  // TODO: Implement the blocking optimization
-  //       (The following is a placeholder naive three-loop dgemm)
-  // column by column matrix multiplication
-  //   for (int i = 0; i < n; ++i) {
-  //     for (int j = 0; j < n; ++j) {
-  //       for(int k = 0; k < n; ++k) {
-  //         C[k+j*n] += A[k+i*n] * B[i+j*n];
-  //       }
-  //     }
-  //   }
-  // }
-
   // blocked matrix multiplication
-  int block_size = 100;
+  int block_size = 200;
+  //int block_size = 146                                                  //opt for L2
+  //int block_size = 36                                                   //opt for L1d
+  //int block_size = 100
   for (int i = 0; i < n; i += block_size) {
     for (int j = 0; j < n; j += block_size) {
       for (int k = 0; k < n; k += block_size) {
