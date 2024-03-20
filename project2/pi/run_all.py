f3dd07708e3a7af42b732225a13f4b25396bee53
Basil Zemann <bzemann@student.ethz.ch>
2024-03-20 13:47:17 +0100

    implemented py script for weak and strong

---
+++ project2/pi/run_all.py
@@ -0,0 +1,111 @@
+import subprocess as sp
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+
+def plot_strong(arr_time_average_ser, arr_time_average_crit, arr_time_average_red, n_threads):
+    speedup_critical = arr_time_average_ser / np.array(arr_time_average_crit)
+    speedup_reduction = arr_time_average_ser / np.array(arr_time_average_red)
+    
+    plt.plot(n_threads, speedup_critical, label='critical')
+    plt.plot(n_threads, speedup_reduction, label='reduction')
+    plt.xlabel('nthreads')
+    plt.ylabel('speedup')
+    plt.legend()
+    plt.title('Strong Scaling')
+    plt.savefig('strong.pdf')
+                      
+def plot_weak(arr_time_average_ser, arr_time_average_crit, arr_time_average_red, n_threads):
+    speedup_critical = np.array(arr_time_average_ser) / np.array(arr_time_average_crit)
+    speedup_reduction = np.array(arr_time_average_ser) / np.array(arr_time_average_red)
+   
+    plt.plot(n_threads, speedup_critical, label='critical')
+    plt.plot(n_threads, speedup_reduction, label='reduction')
+    plt.xlabel('nthreads')
+    plt.ylabel('speedup')
+    plt.legend()
+    plt.title('Weak Scaling')
+    plt.savefig('weak.pdf')
+
+def run_strong(n_threads, exe_path, n_runs, time_average):
+    total_time = 0
+    env = {'OMP_NUM_THREADS': str(n_threads)}
+
+    for _ in range(n_runs):
+        result = sp.run([exe_path], env=env, stdout=sp.PIPE)
+        output = result.stdout.decode('utf-8')
+        print(output) 
+        match = re.search(r'time = ([0-9.]+) secs', output)
+        if match:
+            time_str = match.group(1)
+            time = float(time_str)
+            total_time += time
+            
+    time_average.append(total_time / n_runs)
+
+def run_weak(n_threads, exe_path, n_runs, time_average):
+    total_time = 0
+    env = {'OMP_NUM_THREADS': str(n_threads)}
+
+    for _ in range(n_runs):
+        arg = 1000000 * n_threads
+        arg_str = str(arg)
+        result = sp.run([exe_path, arg_str], env=env, stdout=sp.PIPE)
+        output = result.stdout.decode('utf-8')
+        print(output)
+        
+        match = re.search(r'time = ([0-9.]+) secs', output)
+        if match:
+            time_str = match.group(1)
+            time = float(time_str)
+            total_time += time
+            
+    time_average.append(total_time / n_runs)
+
+if __name__ == "__main__":
+    serial_time_average_weak = []
+    critical_time_average_weak = []
+    reduction_time_average_weak = []
+    
+    serial_time_average_strong = []
+    critical_time_average_strong = []
+    reduction_time_average_strong = []
+    
+    n_threads = []
+
+    print("Strong Scaling")
+    for i in range(6):
+        num_threads = 2 ** i
+        n_threads.append(num_threads)
+        
+        print(f"Running with OMP_NUM_THREADS = {num_threads}")         
+        print("Serial")
+        run_strong(num_threads, "./pi_serial", 10, serial_time_average_strong)
+        
+        print("Critical")
+        run_strong(num_threads, "./pi_omp_critical", 10, critical_time_average_strong)
+        
+        print("Reduction")
+        run_strong(num_threads, "./pi_omp_reduction", 10, reduction_time_average_strong)
+    plot_strong(serial_time_average_strong, 
+                critical_time_average_strong, 
+                reduction_time_average_strong, 
+                n_threads)
+
+    print("Weak Scaling")
+    for i in range(6):
+        num_threads = 2 ** i
+        
+        print(f"Running with OMP_NUM_THREADS = {num_threads}")         
+        print("Serial")
+        run_weak(num_threads, "./pi_serial", 10, serial_time_average_weak)
+        
+        print("Critical")
+        run_weak(num_threads, "./pi_omp_critical", 10, critical_time_average_weak)
+        
+        print("Reduction")
+        run_weak(num_threads, "./pi_omp_reduction", 10, reduction_time_average_weak)
+    plot_weak(serial_time_average_weak, 
+              critical_time_average_weak, 
+              reduction_time_average_weak,
+              n_threads)
---
+++ project2/pi/run_all.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=omp_scaling    # Job name    (default: sbatch)
+#SBATCH --output=pi-scaling.out   # Output file (default: slurm-%j.out)
+#SBATCH --error=pi-scaling.err    # Error file  (default: slurm-%j.out)
+#SBATCH --ntasks=1                # Number of tasks
+#SBATCH --constraint=EPYC_7763    # Select node with CPU
+#SBATCH --cpus-per-task=128       # Number of CPUs per task
+#SBATCH --mem-per-cpu=1024        # Memory per CPU
+#SBATCH --time=01:00:00           # Wall clock time limit
+
+# Load some modules
+module load gcc
+module load python/3.11.6
+
+# Compile
+make clean
+make
+
+python run_all.py
