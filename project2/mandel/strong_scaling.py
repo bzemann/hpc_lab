892b6f486f90a872e6a855635b19dcc8ced1bd7c
Basil Zemann <bzemann@student.ethz.ch>
2024-03-21 23:05:59 +0100

    small changes in python scritpt

--- project2/mandel/strong_scaling.py
+++ project2/mandel/strong_scaling.py
@@ -28,36 +28,6 @@ def run_ser(n_threads, n_runs, exe_path, time_average):
     
     time_average = tot_time / n_runs
 
-#def run_par(n_threads, n_runs, exe_path, time_average):
-#    tot_time = 0
-#    env = {'OMP_NUM_THREADS': str(n_threads)}
-#    for _ in range(n_runs):
-#        result = sp.Popen([exe_path], env=env, stdout=sp.PIPE, bufsize=1, universal_newlines=True)
-#        for line in result.stdout:
-#            print(line, end='')
-#            match = re.match(r'thread id: (\d+), finished in: (\d+\.\d+) seconds\s+, num iterations: (\d+)', line)
-#            if match:
-#                t_id, time_finished, num_iterations = match.groups()
-#                print(f"Thread {t_id}: Finished in {time_finished} seconds, Number of iterations: {num_iterations}")
-#            elif line.startswith("thread:"):
-#                print(line.strip())
-#
-#        result.wait()
-#        if result.returncode != 0:
-#            print(f"Error running {exe_path}. Return code: {result.returncode}")
-#            return
-#
-#        match = re.search(r'Total Time:\s+(\d+.\d+)\s+seconds', result.stdout)
-#        if match:
-#            tot_time += float(match.group(1))
-#        else:
-#            print("Total time not found in the output")
-#            return
-#    
-#    # Calculate average time
-#    time_average.append(tot_time / n_runs)
-
-
 def run_par(n_threads, n_runs, exe_path, time_average):
     tot_time = 0
     env = {'OMP_NUM_THREADS': str(n_threads)}
@@ -81,15 +51,15 @@ if __name__ == "__main__":
     n_threads = []
     
     print("Strong scaling")
-    for i in range(4, -1, -1):
+    for i in range(8):
         num_threads = 2 ** i
         n_threads.append(num_threads)
         
         print(f"Running with OMP_NUM_THREADS = {num_threads}")
         print("Parallel Critical")
-        run_par(num_threads, 4, "./mandel_par_crit", par_crit_tim_avg)
+        run_par(num_threads, 10, "./mandel_par_crit", par_crit_tim_avg)
         
     print("Serial")
-    run_ser(1, 4, "./mandel_seq", serial_time_avg)
+    run_ser(1, 10, "./mandel_seq", serial_time_avg)
 
     plot_strong(n_threads, serial_time_avg, par_crit_tim_avg)
