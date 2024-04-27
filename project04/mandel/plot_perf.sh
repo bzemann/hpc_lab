8b4e20d48a4e020ee572fe6ecb55bf19f013a92f
Basil Zemann <bzemann@eu-login-16.euler.ethz.ch>
2024-04-27 23:45:46 +0200

    project-4: runned performance measure for mandel

--- project04/mandel/plot_perf.sh
+++ project04/mandel/plot_perf.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-module load gnuplot
+module load gcc /4.8.5
+module load gcc/6.3.0
+module load gcc/8.2.0
+module load gnuplot/5.2.0
+
 LD_LIBRARY_PATH=LD_LIBRARY_PATH:/apps/gcc/gcc-6.1.0/lib64 gnuplot perf.gp
