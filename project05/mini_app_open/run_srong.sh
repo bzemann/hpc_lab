8401a009243d650d3f4955381daa928630933c5f
Basil <bz@student-net-vpn-2861.intern.ethz.ch>
2024-05-12 09:32:39 +0200

    small change

--- project05/mini_app_open/run_srong.sh
+++ project05/mini_app_open/run_srong.sh
@@ -28,7 +28,7 @@ for s in "${sizes[@]}"; do
     echo "num threads: $t"
     for repeat in {1..20}; do
       output=$(./main $s 100 0.005)
-      time=$(echo "$output" | grep -oE '[0-9]+\.[0-9]+')
+      time=$(echo "$output" | grep -oE 'simulation took [0-9]+\.[0-9]+ seconds' | awk '{print $4}')
       echo "$s,$t,$time" >> "$OUTPUT_FILE"
     done
   done
